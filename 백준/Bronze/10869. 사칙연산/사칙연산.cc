df3e9135052489e164eeaa910947456c06a6b313
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2023-12-26 18:38:18 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int A = 0;
+    int B = 0;
+    
+    cin >> A >> B;
+    
+    cout << A+B << endl;
+    cout << A-B << endl;
+    cout << A*B << endl;
+    cout << A/B << endl;
+    cout << A%B << endl;
+    
+    return 0;
+}
