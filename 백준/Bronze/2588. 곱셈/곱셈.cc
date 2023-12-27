f06f154be79350d2ba56b96b941bc3b1e9128236
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2023-12-27 17:23:03 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    
+    cout << ((b % 100) % 10) * a << endl;
+    cout << ((b % 100) / 10) * a << endl;
+    cout << (b / 100) * a << endl;
+    cout << b * a << endl;
+    
+    return 0;
+}
