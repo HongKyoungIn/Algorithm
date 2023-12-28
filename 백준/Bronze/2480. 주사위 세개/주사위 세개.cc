3bdf7ac0178692bc0e6258d9e5610a3d2798f7e3
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2023-12-28 21:35:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    int max_num = 0;
+    int same_num = 0;
+    int price;
+    
+    cin >> a >> b >> c;
+    
+    max_num = max(a, b);
+    max_num = max(max_num, c);
+    
+    if(a == b){
+        same_num = a;
+    }
+    else if(a == c){
+        same_num = a;
+    }
+    else if(b == c){
+        same_num = b;
+    }
+    
+    if(same_num == 0) {
+        price = max_num * 100;
+        cout << price << endl;
+    }
+    else {
+        if(a == b && b == c) {
+            price = 10000 + max_num * 1000;
+            cout << price << endl;
+        }
+        else {
+            price = 1000 + same_num * 100;
+            cout << price << endl;
+        }
+    }
+    
+    return 0;
+}
