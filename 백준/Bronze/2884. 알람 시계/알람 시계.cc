42ad8a312a1bf6cefe0d6282f42ce82d7eca2953
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-01-04 00:04:03 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cc
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -1,28 +1,15 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int main() {
-    int hour, minute;
-    
-    cin >> hour >> minute;
-    
-    if(hour > 0) {
-        if(minute >= 45) {
-            cout << hour << " " << minute - 45 << endl;
-        }
-        else {
-            cout << hour - 1 << " " << 60 + (minute - 45) << endl;
-        }
+int main(){
+    int h,m;
+    cin >> h >> m;
+    if (m >= 45){
+        cout << h << " " << m - 45 << endl;
     }
-    else if(hour == 0) {
-        if(minute >= 45) {
-            cout << hour << " " << minute - 45 << endl;
-        }
-        else {
-            cout << "23" << " " << 60 + (minute - 45) << endl;
-        }
+    else{
+        cout << (h + 23) % 24 << " " << 60 + (m - 45) << endl;
     }
     
     return 0;
