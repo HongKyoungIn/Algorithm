c6f591cd5c3988add2ca828d7afb00a526fa9a01
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-01-08 15:48:17 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    int a, b;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a + b << endl;
+    }
+    
+    return 0;
+}
