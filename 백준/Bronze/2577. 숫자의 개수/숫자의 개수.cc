87757a9e38b4a7e3d555159ac08f071cbef9f63d
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-03-11 11:21:28 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    int sum;
+    int count = 0;
+    string str;
+
+    cin >> a >> b >> c;
+
+    sum = a * b * c;
+    str = to_string(sum);
+
+    for(int i = 48; i <= 57; i++) {
+        char ch = i;
+        for(int j = 0; j < str.length(); j++) {
+            if(i == str[j]) {
+                count++;
+            }
+        }
+        if(i == 57) {
+            cout << count;
+        }
+        else {
+            cout << count << endl;
+        }
+        count = 0;
+    }
+}
