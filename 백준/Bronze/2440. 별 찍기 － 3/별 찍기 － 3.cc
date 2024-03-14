8fc486e70563b1808e93996e13c31301d347c7bd
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-03-14 19:51:51 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -3,18 +3,17 @@
 using namespace std;
 
 int main() {
-
 	int n;
-	cin >> n;
+    cin >> n;
+    int temp = n;
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < temp; j++){
+            cout << "*";
+        }
+        temp--;
+        cout << endl;
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		
-		for (int j = 0; j < n - i; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}
 	return 0;
 }
