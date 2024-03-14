c7d13253cb0b5669a48d0f88c34302d4d18c0e1d
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-03-14 19:27:33 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,30 +1,24 @@
 #include <iostream>
-#include <cstring>
+
 using namespace std;
 
 int main() {
+	string str;
+    int pel = 0;
+    
+    cin >> str;
+    
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == str[str.length() - 1 - i]) {
+            pel = 1;
+        }
+        else {
+            pel = 0;
+            break;
+        }
+    }
+    
+    cout << pel;
 
-	char word[101];
-	cin >> word;
-
-	int end = strlen(word);
-
-	for (int i = 0; i < end; i++)
-	{
-		if (word[i] == word[end - i - 1])
-		{
-			if (i >= end / 2)
-			{
-				cout << 1;
-				break;
-			}
-		}
-		else
-		{
-			cout << 0;
-			break;
-		}
-	}
 	return 0;
 }
-
