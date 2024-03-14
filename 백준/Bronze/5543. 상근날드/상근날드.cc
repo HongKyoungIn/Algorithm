826357bf905a17ec3c4a1877f2a9b189cf1f270e
HongKyoungIn <88764725+HongKyoungIn@users.noreply.github.com>
2024-03-14 19:37:26 +0900

    [Bronze IV] Title: 상근날드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5543. 상근날드/상근날드.cc
+++ 백준/Bronze/5543. 상근날드/상근날드.cc
@@ -3,28 +3,26 @@
 using namespace std;
 
 int main() {
+	int hamburger[3];
+    int juice[2];
+    int hmin = 2000;
+    int jmin = 2000;
+    
+    for(int i = 0; i < 3; i++) {
+        cin >> hamburger[i];
+        if(hamburger[i] <= hmin) {
+            hmin = hamburger[i];
+        }
+    }
+    
+    for(int i = 0; i < 2; i++) {
+        cin >> juice[i];
+        if(juice[i] <= jmin) {
+            jmin = juice[i];
+        }
+    }
+    
+    cout << hmin + jmin - 50;
 
-	int b[3], c[2], t[6];
-	int k = 0, min = 4000;
-	cin >> b[0] >> b[1] >> b[2] >> c[0] >> c[1];
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			t[k] = b[i] + c[j] - 50;
-			k++;
-		}
-	}
-
-	for (int i = 0; i < 6; i++)
-	{
-		if (t[i] < min)
-		{
-			min = t[i];
-		}
-	}
-	cout << min;
 	return 0;
 }
-
